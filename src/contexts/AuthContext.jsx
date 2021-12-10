b90f608cfbd8b9ac8548d38de68f878de9f0e638
Elena Myadzeleva <sagva2014@gmail.com>
2021-12-10 19:21:34 +0100

    Fixed navbar

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,48 +1,49 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut  } from 'firebase/auth'
-import { auth } from '../firebase'
+import React, { createContext, useContext, useEffect, useState } from "react";
+import {
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signOut,
+} from "firebase/auth";
+import { auth } from "../firebase";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 const useAuthContext = () => {
-	return useContext(AuthContext)
-}
+	return useContext(AuthContext);
+};
 
 const AuthContextProvider = ({ children }) => {
-
-	const [user, setUser] = useState()
-	const [isLoggedin, setIsLoggedin] = useState(false)
-	
+	const [user, setUser] = useState();
+	const [isLoggedin, setIsLoggedin] = useState(false);
 
 	onAuthStateChanged(auth, (user) => {
 		if (user) {
-			setUser(user.email)
-			setIsLoggedin(true)
-			console.log(`user`, user)
-		  // User is signed in, see docs for a list of available properties
-		  // https://firebase.google.com/docs/reference/js/firebase.User
-		//   const uid = user.uid;
-		  // ...
+			setUser(user.email);
+			setIsLoggedin(true);
+			// console.log(`user`, user)
+			// User is signed in, see docs for a list of available properties
+			// https://firebase.google.com/docs/reference/js/firebase.User
+			//   const uid = user.uid;
+			// ...
 		} else {
-			setIsLoggedin(false)
-		  // User is signed out
-		  // ...
-		  setUser(null)
+			setIsLoggedin(false);
+			// User is signed out
+			// ...
+			setUser(null);
 		}
-	  });
+	});
 
 	const signup = (email, password) => {
-		return createUserWithEmailAndPassword(auth, email, password)
-	}
+		return createUserWithEmailAndPassword(auth, email, password);
+	};
 	const login = async (email, password) => {
-		await signInWithEmailAndPassword(auth, email, password)
-		
-	}
-	
+		await signInWithEmailAndPassword(auth, email, password);
+	};
 
-	const logout = ()=> {
-		signOut(auth)
-	}
+	const logout = () => {
+		signOut(auth);
+	};
 
 	const contextValues = {
 		// here be everything the children needs/should be able to use
@@ -51,14 +52,14 @@ const AuthContextProvider = ({ children }) => {
 		user,
 		logout,
 		setUser,
-		isLoggedin
-	}
+		isLoggedin,
+	};
 
 	return (
 		<AuthContext.Provider value={contextValues}>
 			{children}
 		</AuthContext.Provider>
-	)
-}
+	);
+};
 
-export { useAuthContext, AuthContextProvider as default }
+export { useAuthContext, AuthContextProvider as default };
--- src/pages/partials/Navigation.jsx
+++ src/pages/partials/Navigation.jsx
@@ -1,54 +1,57 @@
-import React, { useEffect } from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import { useAuthContext } from '../../contexts/AuthContext'
-
+import React, { useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const Navigation = () => {
-	const { user } = useAuthContext()
+	const { user } = useAuthContext();
 
 	useEffect(() => {
-		console.log(`user in Navigator`, user)
+		console.log(`user in Navigator`, user);
 	}, [user]);
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
 				<Link to="/" className="navbar-brand">
-					<span role="img" aria-label="A note with a pen">📝</span> Firestore Todos
+					<span role="img" aria-label="A note with a pen">
+						📝
+					</span>{" "}
+					Firestore Todos
 				</Link>
-				
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav" >
+				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav>
+						<NavLink to="/todos" className="nav-link">
+							Todos
+						</NavLink>
 
-						<NavLink to="/todos" className="nav-link">Todos</NavLink>
+						{!user && (
+							<NavLink to="/login" className="nav-link">
+								Login
+							</NavLink>
+						)}
 
-					</Nav>
-					<Nav>
+						{!user && (
+							<NavLink to="/signup" className="nav-link">
+								Signup
+							</NavLink>
+						)}
 
-						<NavLink to="/login" className="nav-link">Login</NavLink>
+						{user && (
+							<NavLink to="/logout" className="nav-link">
+								logout
+							</NavLink>
+						)}
 
-					</Nav>
-					<Nav>
-
-						<NavLink to="/signup" className="nav-link">Signup</NavLink>
-
-					</Nav>
-					{user && <Nav>
-						<NavLink to="/logout" className="nav-link">logout</NavLink>
-					</Nav> }
-					<Nav>
-
-						{user && <span className="nav-link" >{user} </span>}
-
+						{user && <span className="nav-link">{user} </span>}
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
-	)
-}
+	);
+};
 
-export default Navigation
+export default Navigation;
